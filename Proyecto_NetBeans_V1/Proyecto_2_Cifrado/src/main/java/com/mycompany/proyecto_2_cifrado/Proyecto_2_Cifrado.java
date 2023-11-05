6534fde1b2c0c14c63cc335d112f1298fa9e5cfc
Eduardo rojas Gomez <125102804+Eduardo1105rg@users.noreply.github.com>
2023-11-05 12:32:16 -0600

    Add files via upload

---
+++ Proyecto_NetBeans_V1/Proyecto_2_Cifrado/src/main/java/com/mycompany/proyecto_2_cifrado/Proyecto_2_Cifrado.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.proyecto_2_cifrado;
+
+/**
+ *
+ * @author edurg
+ */
+public class Proyecto_2_Cifrado {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
